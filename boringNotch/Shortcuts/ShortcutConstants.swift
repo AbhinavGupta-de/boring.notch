3487ed256b15c8bbbfdc6db112d6677ef9d8cb2b
abhinavgupta-de <abhinav.gupta@outlook.de>
2026-04-17 12:53:43 +0530

    Move toggleSneakPeek to ctrl+opt+h, free cmd+shift+h for ghostty splits

--- boringNotch/Shortcuts/ShortcutConstants.swift
+++ boringNotch/Shortcuts/ShortcutConstants.swift
@@ -13,6 +13,6 @@ extension KeyboardShortcuts.Name {
     static let toggleMicrophone = Self("toggleMicrophone", default: .init(.f5, modifiers: [.function]))
     static let decreaseBacklight = Self("decreaseBacklight", default: .init(.f1, modifiers: [.command]))
     static let increaseBacklight = Self("increaseBacklight", default: .init(.f2, modifiers: [.command]))
-    static let toggleSneakPeek = Self("toggleSneakPeek", default: .init(.h, modifiers: [.command, .shift]))
+    static let toggleSneakPeek = Self("toggleSneakPeek", default: .init(.h, modifiers: [.control, .option]))
     static let toggleNotchOpen = Self("toggleNotchOpen", default: .init(.i, modifiers: [.command, .shift]))
 }
